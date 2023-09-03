0f4927b5c4c376b4111a28a56fd4b08e70143709
VedMarolia <118649989+VedMarolia@users.noreply.github.com>
2023-09-03 10:46:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(550 , 500)
+    canvas.position(700 , 100)
+    
+    video = createCapture(VIDEO)
+    video.size(550 , 500)
+}
+
+function draw(){
+    background("gray")
+}
